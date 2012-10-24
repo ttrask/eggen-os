d36826c441cd90c77fcb4c089537cc91eead657e
trask.thomas <trask.thomas@gmail.com>
2012-10-24 21:35:43 +0000

    TT - Second to last code push for project.
    What needs to be done:
    1 - comment code
    2 - create doc
    3 - turn in project.

--- OS_Proj2/header.h
+++ OS_Proj2/header.h
@@ -7,7 +7,29 @@
 
 #ifndef STOREMAN_H_
 #define STOREMAN_H_
-
-
-
 #endif /* STOREMAN_H_ */
+
+int LoadTable();
+int ForkProcess();
+int KillProcess(int);
+int AllocateMemory();
+int DeallocateSharedMemory();
+char* ProcessMessage(char[]);
+int GetThreadedMessages(int, int[], int[]);
+char* RemoveNewline(char*);
+FILE* OpenLogFile();
+int ReadFromPipe(int pid, int proc_id, int pipe[], char msg[]);
+int WriteToPipe(int pid, int proc_id, int pipe[], char* msg);
+int TABLE_UPDATE(char* id, int val);
+int TABLE_READ(char* id, int* val);
+int WriteToLogFile(FILE* fp, int pid, int proc_id, char* msg,
+		short SendRecieved);
+int CloseLogFile(FILE* fp);
+void ShowCurrentStatus(int);
+pid_t ForkStoreManager();
+
+void GetInputFromUser();
+
+void SetCLIValues(char *arg, int i);
+
+
